d231390681f66be8cad441dfde8a9ad9294ff30e
bryanavalossistemas <bryanavalossistemas@gmail.com>
2025-03-22 18:51:55 -0500

    second commit

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,13 +1,13 @@
 import useStore from '@/store';
 import axios from 'axios';
 
-// const api = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL,
-// });
-
 const api = axios.create({
-  baseURL: 'http://192.168.18.5:4000',
+  baseURL: import.meta.env.VITE_API_URL,
 });
+
+// const api = axios.create({
+//   baseURL: 'http://192.168.18.5:4000',
+// });
 
 api.interceptors.request.use(
   (config) => {
