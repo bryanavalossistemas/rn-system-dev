f4c47af6935dbd01aea42cc17a8c47441062ad75
bryanavalossistemas <bryanavalossistemas@gmail.com>
2025-04-02 15:45:20 -0500

    remove roles

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -9,7 +9,7 @@ export default function HomeView() {
   return (
     <>
       <h1>
-        Uste a iniciado sesion correctamente con el email: {user?.email} y tiene los roles de: {JSON.stringify(user?.roles)}
+        Uste a iniciado sesion correctamente con el email: {user?.email} y tiene los roles de: {JSON.stringify(user?.role)}
       </h1>
       <Button asChild>
         <Link to="/auth/login">Ir al login</Link>
